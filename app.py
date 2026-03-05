dbe9c75de119dea761d24bac059fc67f27e3a7a0
abinayasri563-art <abinayasri563@gmail.com>
2026-03-05 15:49:31 +0530

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/search")
+@app.route("/search", methods=["GET", "POST"])
 def search():
     query = request.args.get("query")
     return f"You searched for: {query}"
