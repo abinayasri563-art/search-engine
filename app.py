e7229ecb8cad89b7c207e06dc5816d878079b749
abinayasri563-art <abinayasri563@gmail.com>
2026-03-05 16:38:23 +0530

    app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/search", methods=["GET", "POST"])
+@app.route("/search", methods=["POST"])
 def search():
-    query = request.args.get("query")
-    return f"You searched for: {query}"
+    query = request.form.get("query")
+    return "You searched for: " + str(query)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
